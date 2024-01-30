9c419e5afbe2fb6a612785a4890926b5e678bc29
roeintheglasses <hrkjangir@gmail.com>
2024-01-31 02:55:52 +0530

    Added Nativewind support | Added Expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Layout() {
+  return (
+    <SafeAreaProvider>
+      <Tabs />
+    </SafeAreaProvider>
+  );
+}
