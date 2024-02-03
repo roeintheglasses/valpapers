9739fc03c21e483de36ff8fae785cecca280d251
roeintheglasses <hrkjangir@gmail.com>
2024-02-03 06:02:34 +0530

    Minor Class Change in Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <View
       style={{ flexDirection: "row" }}
-      className="w-screen h-20 items-center justify-center pt-2 bg-main shadow shadow-black shadow-xl"
+      className="w-screen h-20 items-center justify-center pt-2 bg-main"
     >
       <Text className=" text-highlight-prime font-valo text-4xl">Val</Text>
       <Text className=" text-slate-400 font-valo text-4xl">papers</Text>
