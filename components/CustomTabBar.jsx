a1ec99f6a99cd588e6b026836b3f15947c01d6a8
roeintheglasses <hrkjangir@gmail.com>
2024-02-03 21:45:27 +0530

    Udpated Tabbar Height

--- components/CustomTabBar.jsx
+++ components/CustomTabBar.jsx
@@ -5,7 +5,7 @@ import { MotiView, MotiText } from "moti";
 
 const { height: screenHeight, width: screenWidth } = Dimensions.get("screen");
 const styles = StyleSheet.create({
-  container: { position: "absolute", bottom: 0, left: 0, right: 0, height: 60 },
+  container: { position: "absolute", bottom: 0, left: 0, right: 0, height: 70 },
   blurView: {
     flexDirection: "row",
   },
