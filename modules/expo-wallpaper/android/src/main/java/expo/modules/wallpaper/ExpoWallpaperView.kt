ea971844cb88d7d9b7b594825d76074b105d33b7
roeintheglasses <hrkjangir@gmail.com>
2024-02-11 23:24:30 +0530

    Cleaned Native Module  Code

--- modules/expo-wallpaper/android/src/main/java/expo/modules/wallpaper/ExpoWallpaperView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.wallpaper
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoWallpaperView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- modules/expo-wallpaper/index.ts
+++ modules/expo-wallpaper/index.ts
@@ -1,17 +1,6 @@
-import {
-  NativeModulesProxy,
-  EventEmitter,
-  Subscription,
-} from "expo-modules-core";
-
 // Import the native module. On web, it will be resolved to ExpoWallpaper.web.ts
 // and on native platforms to ExpoWallpaper.ts
 import ExpoWallpaperModule from "./src/ExpoWallpaperModule";
-import ExpoWallpaperView from "./src/ExpoWallpaperView";
-import {
-  ChangeEventPayload,
-  ExpoWallpaperViewProps,
-} from "./src/ExpoWallpaper.types";
 
 export function hello(): string {
   return ExpoWallpaperModule.hello();
@@ -20,5 +9,3 @@ export function hello(): string {
 export function setWallpaper(options): string {
   return ExpoWallpaperModule.setWallpaper(options);
 }
-
-export { ExpoWallpaperView, ExpoWallpaperViewProps, ChangeEventPayload };
